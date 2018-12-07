f66c2f6a5676fa05378e5b6659997cc4d7378cf3
Passion-4-Life <yinbowen1996@gmail.com>
2018-12-06 22:53:13 -0500

    update file

--- Src/main.py
+++ Src/main.py
@@ -2,8 +2,8 @@
 from collections import Counter
 #from whoosh.index import create_in
 #from whoosh.fields import *
-from Const import BOUND, ideal_length, path_to_FCA_abstract_raw, path_to_data_FCA_fulltext
-import myBM25, myLDA, ProcessDoc
+#from Const import BOUND, ideal_length, path_to_FCA_abstract_raw, path_to_data_FCA_fulltext
+import myBM25, myLDA, Const
 
 def count_lda_result(lda_result):
     count = {}
@@ -13,7 +13,7 @@ def count_lda_result(lda_result):
 
 def main(queries, label):
 
-    with open(path_to_FCA_abstract_raw) as f:
+    with open(Const.path_to_FCA_abstract_raw) as f:
         lines = f.readlines()
 
     #queries = "1 2 3 4"
@@ -23,26 +23,27 @@ def main(queries, label):
     final_query = queries + " " + labels
     result = myBM25.BM25_score(lines, final_query)
     id_result = [int(item[0]) for item in result]
-    print(id_result)
+    #print(id_result)
     
-    if len(id_result) > BOUND:
+    if len(id_result) > Const.BOUND:
         ranks = id_result
     elif len(id_result) == 0:
         lda_result = myLDA.myLDA(final_query.split())
-        ranks = [item[0] for item in lda_result[:ideal_length]]
+        ranks = [item[0] for item in lda_result[:Const.ideal_length]]
     else:
         ranks = id_result
         cou = Counter()
         for i in id_result:
             lda_result = myLDA.myLDA(lines[i].split())
             cou += count_lda_result(lda_result)
-        potential_list = cou.most_common(ideal_length)
+        potential_list = cou.most_common(Const.ideal_length)
         for i in potential_list:
             if i[0] not in ranks:
                 ranks.append(i[0])
     
-    docu_list = ProcessDoc.DocList(path_to_data_FCA_fulltext)
-    return docu_list
+    return ranks
+    #docu_list = ProcessDoc.DocList(path_to_data_FCA_fulltext)
+    #return docu_list
 
 ranks = main("trade", [])
 print(ranks)
